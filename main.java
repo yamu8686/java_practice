4568ec4b76266778b76d311ad23d06c621496265
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-04 08:00:55 +0000

    first_commit

---
+++ main.java
@@ -0,0 +1,8 @@
+class main {
+  public static void main(String[] args) {
+    // for文を用いて、繰り返し処理をつくってください
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i + "回目のループです");
+    }
+  }
+}
