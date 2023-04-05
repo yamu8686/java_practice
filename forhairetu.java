397f891cd6dc99fca6003ef8fc1139573242bc43
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-05 07:58:46 +0000

    javaの練習

---
+++ forhairetu.java
@@ -0,0 +1,10 @@
+class main {
+  public static void main(String[] args) {
+    String[] names = {"Taro","john","Mia"};
+
+    for(String name: names){
+       System.out.println(name);
+    }
+
+  }
+}
---
+++ hairetu.java
@@ -0,0 +1,10 @@
+class main {
+  public static void main(String[] args) {
+    String[] names = {"Taro","john","Mai"};
+
+    for(int i=0; i < names.length; i++ ){
+       System.out.println(names[i]);
+    }
+
+  }
+}
