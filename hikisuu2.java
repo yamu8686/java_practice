878057d388a6f643be3c0bd305d1284dab579d2f
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-07 07:40:49 +0000

    複数の引数

---
+++ hikisuu2.java
@@ -0,0 +1,14 @@
+class main {
+  public static void main(String[] args) {
+    printData("Goemonn", 50);
+
+    printData("Jack", 30);
+
+    printData("Locus", 20);
+  }
+
+  public static void printData(String name, int age) {
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+  }
+}
