167df1070bce8115b16702cbae633a44f7c463b9
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-07 07:29:37 +0000

    引数を使う

---
+++ hikisuu.java
@@ -0,0 +1,13 @@
+class main {
+  public static void main(String[] args) {
+    printData("Goemonn");
+
+    printData("Jack");
+
+    printData("Locus");
+  }
+
+  public static void printData(String name) {
+    System.out.println("私の名前は" + name + "です");
+  }
+}
