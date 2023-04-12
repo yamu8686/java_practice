7b8399d8a6e851dca710f0dd3b10e58a1e395930
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-12 12:28:13 +0000

    TrueFalse

---
+++ truefalse.java
@@ -0,0 +1,10 @@
+class main {
+  public static void main(String[] args) {
+    System.out.println(true);
+    System.out.println(false);
+    System.out.println(12 / 4 == 3);
+    System.out.println(12 / 4 != 3);
+    boolean bool = true;
+    System.out.println(bool);
+  }
+}
