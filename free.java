79e2019b5dd41ad05e35302c5c7032ff2f6f6fa3
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-13 09:03:09 +0000

    練習

---
+++ free.java
@@ -0,0 +1,14 @@
+class main {
+  public static void main (String[] args) {
+    int n = 1;
+
+    switch (n) {
+      case 1:
+        System.out.println("大吉です");
+        break;
+      case 2:
+        System.out.println("吉です");
+        break;
+    }
+  }
+}
