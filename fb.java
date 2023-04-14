f4c03b6e657361ec9a6bd29c9eb6a543d1c0da53
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-14 09:49:05 +0000

    forFizzBuzz

--- fb.java
+++ fb.java
@@ -1,9 +1,7 @@
 class main {
   public static void main (String[] args) {
 
-    int i = 0;
-    while (i < 100) {
-      i++;
+    for (int i = 1; i < 100; i++) {
       if (i % 15 == 0) {
         System.out.println("FizzBuzz");
       } else if (i % 5 == 0) {
