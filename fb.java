1a7a14bd643c565a2dcdb0c57b41a157d1358987
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-14 09:45:11 +0000

    fizzbuzz2

---
+++ fb.java
@@ -0,0 +1,18 @@
+class main {
+  public static void main (String[] args) {
+
+    int i = 0;
+    while (i < 100) {
+      i++;
+      if (i % 15 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      } else if (i % 3 == 0) {
+        System.out.println("Fizz");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}
